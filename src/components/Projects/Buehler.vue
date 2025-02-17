25f45f1d5c025c1297c0a9731da26b63383aec3d
NoPlans4Tonight <jlochan53@gmail.com>
2025-02-17 14:58:05 -0600

    projects - refactor and add css for better visuals

--- src/components/Projects/Buehler.vue
+++ src/components/Projects/Buehler.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="project">
     <h2>Buehler</h2>
-    <p>
-      Built public helpdesk website to traffic customer service requests built
-      on Laravel framework with HTML, JavaScript, and Bootstrap 4 frontend.
-    </p>
     <ul>
+      <li>
+        Built public helpdesk website to traffic customer service requests built
+        on Laravel framework with HTML, JavaScript, and Bootstrap 4 frontend.
+      </li>
       <li>
         Built web application that improved production and assembly of hardness
         testing machines by 40%; this web application replaced an old process
@@ -56,25 +56,71 @@ export default {
 .project {
   margin: 20px 0;
   padding: 20px;
-  border: 1px solid #ccc;
+  border: 1px solid #333;
   border-radius: 8px;
+  background: #181818;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease;
 }
 
-.frame {
-  display: flex;
-  justify-content: center;
+.project:hover {
+  transform: translateY(-10px);
+}
+
+h2 {
+  font-family: "Roboto", sans-serif;
+  font-size: 24px;
+  color: #fff;
+  text-align: center;
   margin-bottom: 20px;
 }
 
-.pics {
-  display: flex;
-  justify-content: center;
+p {
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
+  color: #ccc;
+  line-height: 1.6;
+  margin-bottom: 20px;
 }
 
-img {
-  width: 100%;
-  height: auto;
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
+  color: #ccc;
+  line-height: 1.6;
+  margin-bottom: 20px;
+  position: relative;
+  padding: 15px 20px;
+  background: #222;
   border-radius: 8px;
-  max-width: 600px; /* Adjust the max-width as needed */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition:
+    transform 0.3s ease,
+    background 0.3s ease;
+}
+
+li:hover {
+  transform: translateY(-5px);
+  background: #333;
+}
+
+li::before {
+  content: "";
+  position: absolute;
+  left: -10px;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 10px;
+  height: 10px;
+  background: #0bceaf;
+  border-radius: 50%;
+  box-shadow:
+    0 0 0 2px #222,
+    0 0 0 4px #0bceaf;
 }
 </style>
